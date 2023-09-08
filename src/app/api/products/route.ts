3e0c41c77ae0814922f96233f2a788069c2e1eb5
Jiří Burdych <jiri.burdych@astratex.cz>
2023-09-08 20:41:00 +0200

    asdf

--- src/app/api/products/route.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from '@/lib/prisma'
-import { Product } from '@prisma/client'
-import { NextResponse } from 'next/server'
- 
-export async function GET() {
-  const data = await prisma.product.findMany()
-  return NextResponse.json( data )
-}
