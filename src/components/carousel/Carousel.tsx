6df8ec4117ad48384841a34ac5e0718b39c1cd9f
Jirka Burdych <burdik6@gmail.com>
2023-09-10 12:20:13 +0200

    carousel arrows

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -12,6 +12,8 @@ import {
   MoveLeft,
   MoveRight,
 } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Button } from "../ui/button";
 
 interface Props {
   title: string;
@@ -34,23 +36,37 @@ export const Carousel = (props: Props) => {
     <section>
       <div className="relative flex flex-col">
         <H1>{props.title}</H1>
-        <div className="flex flex-row gap-2 justify-center items-center md:absolute md:bottom-0 md:right-2 mb-4">
+        {/* <div className="flex flex-row gap-2 justify-center items-center md:absolute md:bottom-0 md:right-2 mb-4">
           <MoveLeft onClick={scrollPrev} className="cursor-pointer" />
           <MoveRight onClick={scrollNext} className="cursor-pointer" />
-        </div>
+        </div> */}
       </div>
-      <div className={style.embla} ref={ref}>
-        <div className={style.emblaContainer}>
-          {props.children.map((child, idx) => (
-            <div
-              key={idx}
-              className={style.emblaSlide}
-              style={{ width: `${100 / (props.slidesCount ?? 1)}%` }}
-            >
-              {child}
-            </div>
-          ))}
-          {/* <CarouselItem style={style.emblaSlide} /> */}
+      <div className="relative h-full">
+        <Button
+          onClick={scrollPrev}
+          className="rounded-full bg-primary absolute -left-5 z-[33] top-1/2 -translate-y-1/2"
+        >
+          <MoveLeft className="cursor-pointer" />
+        </Button>
+        <Button
+          onClick={scrollNext}
+          className="rounded-full bg-primary absolute -right-5 z-[33] top-1/2 -translate-y-1/2"
+        >
+          <MoveRight className="cursor-pointer" />
+        </Button>
+        <div className={style.embla} ref={ref}>
+          <div className={style.emblaContainer}>
+            {props.children.map((child, idx) => (
+              <div
+                key={idx}
+                className={style.emblaSlide}
+                style={{ width: `${100 / (props.slidesCount ?? 1)}%` }}
+              >
+                {child}
+              </div>
+            ))}
+            {/* <CarouselItem style={style.emblaSlide} /> */}
+          </div>
         </div>
       </div>
     </section>
